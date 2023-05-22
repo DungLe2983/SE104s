17f9c7907ef14cf76006f20706e26cd71cd3a426
DungLe2983 <lequocdung2983@gmail.com>
2023-05-22 10:55:40 +0700

    a

--- SE104_Proj/PhongMachTu/obj/Debug/net6.0-windows/LoginWindow.g.i.cs
+++ SE104_Proj/PhongMachTu/obj/Debug/net6.0-windows/LoginWindow.g.i.cs
@@ -133,7 +133,7 @@ namespace PhongMachTu {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/PhongMachTu;component/loginwindow.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/PhongMachTu;V1.0.0.0;component/loginwindow.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\LoginWindow.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
